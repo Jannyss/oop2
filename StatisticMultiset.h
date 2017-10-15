17f1a7f38a23874bbbf237e6cca36cb8c664fa48
Jannyss <jannyss@mail.ru>
2017-10-15 02:15:05 -1200

    Add files via upload
    Added templates

--- StatisticMultiset.h
+++ StatisticMultiset.h
@@ -1,47 +1,98 @@
 #ifndef STATISTICMULTISET_H_INCLUDED
 #define STATISTICMULTISET_H_INCLUDED
+#endif // STATISTICMULTISET_H_INCLUDED
 #pragma once
 #include <set>
 #include <limits>
 #include <vector>
 #include <list>
-#endif // STATISTICMULTISET_H_INCLUDED
+#include <numeric>
+#include <fstream>
+
 using namespace std;
+template<class T>
 
 class StatisticMultiset {
 public:
-    StatisticMultiset();
-    ~StatisticMultiset();
     // Добавляет число в набор.
-    void AddNum( int num );
+    void AddNum( const T &num ) {
+        statmset.insert(num);
+        if (num > maxvalue) maxvalue = num;
+        if (num < minvalue) minvalue = num;
+        avgvalue = accumulate(statmset.begin(), statmset.end(), 0)/statmset.size();
+    };
     // Данные из multiset-а
-    void AddNum( const multiset<int>& numbers );
-    // Данные из вектора
-    void AddNum( const vector<int>& numbers );
-    // Данные из списка
-    void AddNum( const list<int>& numbers );
-    // Данные из другого набора
-    void AddNums( const StatisticMultiset& a_stat_set );
-    // Данные из файла
-    void AddNumsFromFile( const char* filename );
-    // Максимальное число в наборе.
-    int GetMax() const;
-    // Минимальное число в наборе.
-    int GetMin() const;
-    // Среднее арифметическое всего набора.
-    float GetAvg() const;
-    // Кол-во чисел в наборе меньше заданного порога.
-    int GetCountUnder( float threshold ) const;
-    // Кол-во чисел в наборе больше заданного порога.
-    int GetCountAbove( float threshold ) const;
-    // Содержимое набора
-    void ShowMultiset() const;
+    void AddNum( const multiset<T>& numbers ) {
+        for (typename multiset<T>::iterator it = numbers.begin(); it != numbers.end(); ++it) {
+            StatisticMultiset::AddNum(*it);
+        }
+    }
+
+    void AddNum (const vector<T>& numbers) {
+    for (typename vector<T>::const_iterator it = numbers.begin(); it != numbers.end(); ++it) {
+            StatisticMultiset::AddNum(*it);
+        }
+    }
+
+    void AddNum ( const list<T>& numbers ) {
+        for (typename list<T>::const_iterator it = numbers.begin(); it != numbers.end(); ++it) {
+            StatisticMultiset::AddNum(*it);
+        }
+    }
+
+    void AddNums( const StatisticMultiset& a_stat_set ) {
+        for (typename multiset<T>::iterator it = a_stat_set.statmset.begin(); it != a_stat_set.statmset.end(); ++it) {
+            StatisticMultiset::AddNum(*it);
+        }
+    }
+
+    void AddNumsFromFile( const char* filename ) {
+        ifstream datafile;
+        datafile.open(filename);
+        while (!datafile.eof()) {
+            T num;
+            datafile >> num;
+            StatisticMultiset::AddNum(num);
+        }
+    }
+
+    int GetMax() const {
+        return maxvalue;
+    }
+
+    int GetMin() const {
+        return minvalue;
+    }
+
+    float GetAvg() const {
+        return avgvalue;
+    }
+
+    int GetCountUnder(float threshold) const {
+        typename multiset<T>::iterator itlow = statmset.lower_bound(threshold);
+        int countel = 0;
+        for (typename multiset<T>::iterator it = statmset.begin(); it != itlow; ++it) countel++;
+        //if (statmset.find(threshold) != statmset.end()) countel -= 1;
+        return countel;
+    }
+
+    int GetCountAbove(float threshold) const {
+        typename multiset<T>::iterator itup = statmset.upper_bound(threshold);
+        int countel = 0;
+        for (typename multiset<T>::iterator it = itup; it != statmset.end(); ++it) countel++;
+        //if (statmset.find(threshold) != statmset.end()) countel -= 1;
+        return countel;
+    }
+
+    void ShowMultiset() const {
+        for (typename multiset<T>::iterator it = statmset.begin(); it != statmset.end(); ++it) {
+            cout << *it << " ";
+        }
+}
 
 private:
-    multiset<int> statmset;
-    int maxvalue = numeric_limits<int>::min();
-    int minvalue = numeric_limits<int>::max();
+    multiset<T> statmset;
+    T maxvalue = numeric_limits<T>::min();
+    T minvalue = numeric_limits<T>::max();
     float avgvalue;
 };
-
-
